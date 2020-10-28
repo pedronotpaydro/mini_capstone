c54a5af583ae57c9df4a4910eb9ac3cc986a3d3e
Pedro <orsomethingelse@gmail.com>
2020-10-28 14:30:31 -0500

    added all products view

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,10 @@
 class Api::ProductsController < ApplicationController
+  def all_products_action
+    @products = Product.all
+    render "all.json.jb"
+  end
+  
+  def first_product_action
+    @first = Product.first
+    render "first.json.jb"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products" => "products#all_products_action"
+
+    get "/first_product" => "products#first_product_action"
+  end
 end
